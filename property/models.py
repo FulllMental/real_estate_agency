5a568a2fa44b09cb198332295e2192c86d67cd44
FulllMental <104234625+FulllMental@users.noreply.github.com>
2022-12-10 00:42:25 +0300

    Fill Owner.flat model

--- property/models.py
+++ property/models.py
@@ -5,7 +5,7 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
-    owner = models.CharField('ФИО владельца', max_length=200)
+    owner = models.CharField('ФИО владельца', db_index=True, max_length=200)
     owners_phonenumber = models.CharField('Номер владельца', max_length=20)
     owner_pure_phone = PhoneNumberField(blank=True,
                                         region='RU',
