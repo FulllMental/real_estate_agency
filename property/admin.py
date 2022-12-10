78af5e368600d217fe4ca2e73f82463b44ae52bd
FulllMental <104234625+FulllMental@users.noreply.github.com>
2022-12-10 14:09:53 +0300

    Add PropertyInline in Admin Panel

--- property/admin.py
+++ property/admin.py
@@ -3,13 +3,28 @@ from django.contrib import admin
 from .models import Flat, Complain, Owner
 
 
+class PropertyInline(admin.TabularInline):
+    model = Owner.flat.through
+    raw_id_fields = ['owner', 'flat']
+
+
 @admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
-    search_fields = ['town', 'address', 'owner']
+    search_fields = ['town',
+                     'address',
+                     'owner']
     readonly_fields = ['created_at']
-    list_display = ['address', 'price', 'new_building', 'construction_year', 'town', 'owner_pure_phone', 'owners_phonenumber']
+    list_display = ['address',
+                    'price',
+                    'new_building',
+                    'construction_year',
+                    'town',
+                    'owner_pure_phone',
+                    'owners_phonenumber']
     list_editable = ['new_building']
-    list_filter = ['new_building', 'rooms_number', 'has_balcony']
+    list_filter = ['new_building',
+                   'rooms_number',
+                   'has_balcony']
     raw_id_fields = ['liked_by']
 
 
@@ -21,4 +36,7 @@ class ComplainsAdmin(admin.ModelAdmin):
 
 @admin.register(Owner)
 class OwnerAdmin(admin.ModelAdmin):
-    raw_id_fields = ['flat']
+    inlines = [
+        PropertyInline
+    ]
+    exclude = ['flat']
